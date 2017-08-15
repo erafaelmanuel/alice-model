8780edd033010a1e629bccd7a60c1615d42ee91d
Rafael Manuel <erafaelmanuel@gmail.com>
2017-08-15 17:46:12 +0800

    Update jersey dependency.

--- src/main/java/com/remswork/project/alice/model/Teacher.java
+++ src/main/java/com/remswork/project/alice/model/Teacher.java
@@ -3,7 +3,16 @@ package com.remswork.project.alice.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
