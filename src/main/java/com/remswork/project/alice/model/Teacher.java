a7b144f8fd234708a7dae61a6fc519d8b33bb736
Rafael Manuel <erafaelmanuel@gmail.com>
2017-08-15 13:42:26 +0800

    Create a model for Alice Project.

---
+++ src/main/java/com/remswork/project/alice/model/Teacher.java
@@ -0,0 +1,115 @@
+package com.remswork.project.alice.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@Table(name="tblteacher")
+public class Teacher {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private String email;
+	@OneToOne
+	@JoinColumn(name="userDetailId")
+	private UserDetail userDetail;
+	@ManyToOne(cascade={CascadeType.ALL})
+	@JoinColumn(name="departmentId")
+	private Department department;
+	@Transient
+	private List<Link> links;
+	
+	public Teacher() {
+		super();
+		links = new ArrayList<>();
+	}
+	
+	public Teacher(String firstName, String lastName, String middleName, String email) {
+		this();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.middleName = middleName;
+		this.email = email;
+	}
+
+	public Teacher(long id, String firstName, String lastName, String middleName, String email) {
+		this(firstName, lastName, middleName, email);
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public UserDetail getUserDetail() {
+		return userDetail;
+	}
+
+	public void setUserDetail(UserDetail userDetail) {
+		this.userDetail = userDetail;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+
+	public void addLink(Link link) {
+		links.add(link);
+	}
+}
